d97fad7182b688af73ea5bf6a40fbccd061b1774
Mark J. Smith <mjsmith101@gmail.com>
2022-04-01 16:17:38 -0400

    Update footer year

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 function Footer() {
+  const footerYear = new Date().getFullYear()
+
   return (
     <div>
       <footer id='footer' className='text-center py-1'>
         <div className="container">
-          <p>Copyright &copy; MJS Dive Log - 2022</p>
+          <p>Copyright &copy; MJS Dive Log - {footerYear}</p>
         </div>
       </footer>
     </div>
